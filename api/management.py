c4d17f86471dc1f3642b7f55b35b2eba1f622d94
sahil.v@cisinlabs.com <sahil.v@cisinlabs.com>
2025-04-02 17:53:19 +0530

    updated management.py

--- api/management.py
+++ api/management.py
@@ -1,5 +1,6 @@
 import click
 import uvicorn
+import subprocess
 
 @click.group()
 def cli():
@@ -7,8 +8,19 @@ def cli():
 
 @cli.command()
 def start():
-    "Start the FastAPI server"
+    """ Start the FastAPI server """
     uvicorn.run("src.core.main:app", host="0.0.0.0", port=8000, reload=True)
+
+@cli.command()
+@click.argument("message", required=True)
+def makemigration(message):
+    """ create a new alembic migration """
+    subprocess.run(["alembic", "revision", "--autogenerate", "-m", message], check=True)
+
+@cli.command()
+def migrate():
+    """ Applying migration"""
+    subprocess.run(["alembic", "upgrade", "head"], check=True)
 
 if __name__ == "__main__":
     cli()
